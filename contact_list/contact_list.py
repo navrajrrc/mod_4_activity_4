1e2244d140732c820cc1127b015bdfbb22538c64
Navraj Singh <navrajsingh@rrc.ca>
2025-03-28 13:54:16 -0500

    Done the method for on_Add_contact

--- contact_list/contact_list.py
+++ contact_list/contact_list.py
@@ -5,6 +5,8 @@ __version__ = "1.0.0"
 __credits__ = ""
 
 from PySide6.QtWidgets import  QMainWindow, QLineEdit, QPushButton, QTableWidget, QLabel, QVBoxLayout, QWidget, QTableWidgetItem
+from PySide6.QtCore import Slot 
+from PySide6.QtWidgets import QMessageBox 
 
 class ContactList(QMainWindow):
     """Represents a window that provides the UI to manage contacts."""
@@ -48,3 +50,28 @@ class ContactList(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+
+    @Slot()
+    def on_add_contact(self):
+        """
+        This method will handle the addition of new contact
+        and if the contact name or number is not valid it will send a message.
+        """
+        contact_name = self.contact_name_input.text()
+        phone_number = self.phone_input.text()
+        
+        row_position = self.contact_table.rowCount()
+        self.contact_table.insertRow(row_position)
+
+        name_item = QTableWidgetItem(contact_name)
+        phone_item = QTableWidgetItem(phone_number)
+
+        self.contact_table.setItem(row_position, 0, name_item)
+        self.contact_table.setItem(row_position, 1, phone_item)
+
+        if not contact_name or phone_number:
+            self.status_label.setText(f"Please enter a contact name and phone number")
+        else:
+            self.status_label.setText(f"Added contact: {contact_name}")
+
+        
